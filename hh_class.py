840b2ad041fc239f55bbaf8ec86926df3d706254
NikitinSemen <pr.pypblrka@gmail.com>
2024-04-20 18:33:24 +0400

    попытка создать классы, методы

--- hh_class.py
+++ hh_class.py
@@ -2,12 +2,34 @@ import psycopg2
 
 
 class GetDataFromDataBase:
-    def __init__(self):
-        pass
 
+    def get_data_from_database(self):
+        conn = psycopg2.connect(dbname=self.data_base_name, **self.params)
+        conn.autocommit = True
+        data = []
+        with conn.cursor() as cur:
+            cur.execute('SELECT company_name,  FROM company;')
+            vacancy_data = cur.fetchall()
+            data.append(vacancy_data)
+            return data
+
+
+class VacancyCompanyHh:
+    def __init__(self, data_base_name, params: dict):
+        self.data_base_name = data_base_name
+        self.params = params
+        self.conn = psycopg2.connect(dbname=self.data_base_name, **self.params)
+        self.conn.autocommit = True
+        self.mag = 'МАГНИТ'
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
+        with self.conn.cursor() as cur:
+            cur.execute(
+                f'SELECT DISTINCT company_name, COUNT(ads_name) FROM company '
+                f'INNER JOIN ads USING(company_id) WHERE company_name IN ({self.mag}) GROUP BY company_name')
+
+            result = cur.fetchall()
+            return result
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с названием компании, названием вакансии, зарплатой и ссылкой на вакансию"""
