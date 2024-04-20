7253eabae3d27a28adf27083e38915b758aeb172
NikitinSemen <pr.pypblrka@gmail.com>
2024-04-20 08:49:14 +0400

    create class GetDataFromDataBase

---
+++ hh_class.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+class GetDataFromDataBase:
+    def __init__(self):
+        pass
+
+    def get_companies_and_vacancies_count(self):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        pass
+
+    def get_all_vacancies(self):
+        """Получает список всех вакансий с названием компании, названием вакансии, зарплатой и ссылкой на вакансию"""
+        pass
+
+    def get_avg_salary(self):
+        """ Получает среднюю зарплату по вакансиям"""
+        pass
+
+    def get_vacancies_with_keyword(self):
+        """Получает список вакансий по ключевому слову"""
+        pass
