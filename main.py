adc66e0dcf622a18892b452e3f1274e76e83d249
NikitinSemen <pr.pypblrka@gmail.com>
2024-04-22 00:04:21 +0400

    написаны блоки проверки работы методов

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pprint import pprint
-from utils import save_data_to_database
-from utils import create_data_base
-from config import config
-from utils import get_data_from_hh
-import json
+from src.utils import save_data_to_database
+from src.utils import create_data_base
+from src.config import config
+from src.utils import get_data_from_hh
+from src.hh_class import VacancyCompanyHh\
 
+# список id компаний для получения данных с headhunter
 id_company_hh = [
     '1942330',  # Пятерочка
     '49357',  # Магнит розничная сеть
@@ -16,14 +17,25 @@ id_company_hh = [
     '1834129',  # White Rabbit Family
     '2120',  # Азбука вкуса
     '87021',  # WILDBERRIES
-
 ]
+# переменная с конфигурацией для входа в базу данных
 params = config()
+
+# блок для получения данных с api headhunter, создания базы данных и таблиц,
+# сохранения данных полученных с api в созданные таблицы
 data_from_hh = get_data_from_hh(id_company_hh)
 create_data_base('headhunter', params)
 save_data_to_database(data_from_hh, 'headhunter', params)
 
-# for i in data_from_hh:
-#     pprint(i['vacancy'])
-#     # print(i['vacancy'])
-
+# блок для проверки работы методов класса VacancyCompanyHh
+inf_for_data_base = VacancyCompanyHh('headhunter', params)
+full_inf_about_ads = inf_for_data_base.get_all_vacancies()
+inf_about_count_vacancy = inf_for_data_base.get_companies_and_vacancies_count()
+for k, v in inf_about_count_vacancy.items():
+    print(f'Компания {k}, вакансий {v}')
+pprint(full_inf_about_ads)
+avg_salary = inf_for_data_base.get_avg_salary()
+print(round(avg_salary[0][0]))
+user_input = input()
+search_by_keyword = inf_for_data_base.get_vacancies_with_keyword(user_input)
+print(search_by_keyword)
